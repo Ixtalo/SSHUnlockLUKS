65a3583d3c821a1940c80b69ee8546e958955db2
Ixtalo <ixtalo@users.noreply.github.com>
2023-05-06 08:22:45 +0200

    socket check and CSV control

--- README.md
+++ README.md
@@ -29,4 +29,28 @@ This Python program does that - it sends the LUKS password via SSH.
 3. `cp env.template .env` and configure IP, port, keyfile location and passphrase
 4. `pipenv run ssh-unlock-luks.py`
 5. setup cron job:
-   `0 */4 * * *    curl --fail --silent --head --user-agent "SSHUnlockLUKS" http://SSH_HOST_IP/ssh_unlock_luks.txt >/dev/null || (cd /opt/SSHUnlockLUKS/ && .venv/bin/python ssh_unlock_luks.py)`
+   `0 */4 * * *    (cd /opt/SSHUnlockLUKS/ && .venv/bin/python ssh_unlock_luks.py 2>/dev/null)`
+
+
+## CSV Control
+
+When the variable `SUL_CONTROL_CSV_URL` is set then it will be downloaded as CSV and checked for matching hosts to be unlocked.
+The motivation here is to control whether to actually unlock or not, to have influence.
+Sometimes unlocking should not happen in every case, but in a controlled fashion.
+For instance, a VPS should only be unlocked when the superuser/owner gives his o.k.
+
+How-To:
+
+1. Create a CSV file with the 2 columns: HOST,UNLOCK_FLAG
+2. Place this CSV file on an accessible web-server.
+3. Adjust the variable `SUL_CONTROL_CSV_URL` in `.env`.
+
+CSV example:
+
+| HOST       | UNLOCK_FLAG |
+|------------|-------------|
+| 1.2.3.4    | yes         |
+| 11.22.33.1 | no          |
+
+`HOST` must match `SUL_SSH_IP`.
+`UNLOCK_FLAG` could be yes/1/true.
--- ssh_unlock_luks.py
+++ ssh_unlock_luks.py
@@ -4,15 +4,17 @@
 import os
 import sys
 import logging
+import socket
 from time import sleep
 from pathlib import Path
+import requests
 from dotenv import load_dotenv
 # https://docs.paramiko.org/en/stable/api/client.html
 from paramiko import SSHClient
 
-__version__ = "1.1.2"
+__version__ = "1.3.0"
 __date__ = "2021-12-15"
-__updated__ = "2022-10-16"
+__updated__ = "2023-05-03"
 __author__ = "Ixtalo"
 __license__ = "AGPL-3.0+"
 __email__ = "ixtalo@gmail.com"
@@ -37,6 +39,58 @@ assert os.getenv("SUL_SSH_IP"), "Missing configuration!"
 assert os.getenv("SUL_SSH_PORT"), "Missing configuration!"
 assert os.getenv("SUL_LUKS_PASS"), "Missing configuration!"
 
+
+host = os.getenv("SUL_SSH_IP")
+port = int(os.getenv("SUL_SSH_PORT"))
+username = os.getenv("SUL_SSH_USER", "root")
+
+
+# check if the remote server is actually in dropbear-initramfs mode
+logging.debug("Trying to connect to %s:%s ...", host, port)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.settimeout(1)     # 1 seconds timeout
+    s.connect((host, port))
+    data = s.recv(16)   # bufsize should be power of 2
+logging.debug("remote server data: %s", repr(data))
+if not data.startswith(b"SSH-2.0-dropbear"):
+    logging.warning("No Dropbear-SSH remote endpoint! Nothing to do.")
+    sys.exit(0)
+
+
+# check external CSV "control server"
+csv_control_url = os.getenv("SUL_CONTROL_CSV_URL", "")
+if csv_control_url:
+    logging.info("Checking '%s' ...", csv_control_url)
+    # HTTP GET
+    r = requests.get(csv_control_url)
+    logging.debug("HTTP GET: %s", str(r))
+    if not r.ok:
+        logging.error("Problem getting CSV control data: %s (%s)", r.reason, str(r))
+        sys.exit(1)
+    # check each line for a match
+    lines = r.content.decode().splitlines()
+    do_unlock = False
+    for line in lines:
+        # expect CSV
+        try:
+            csv_host, unlock_flag = line.split(",")[0:2]
+        except ValueError as ex:
+            logging.error("Ignoring invalid CSV line: '%s'", line)
+            continue
+        logging.debug("line: %s -> %s", csv_host, unlock_flag)
+        if csv_host.strip() == host:
+            # match found, check if it should actually be unlocked
+            if unlock_flag.lower() in ("yes", "1", "true"):
+                do_unlock = True
+                break
+    # URL is given but no yes-unlocking-flag is found
+    if do_unlock:
+        logging.info("SUL_CONTROL_CSV_URL says to unlock %s", host)
+    else:
+        logging.error("SUL_CONTROL_CSV_URL is given but no enabled unlocking host is found! Abort.")
+        sys.exit(2)
+
+
 # SSH client
 client = SSHClient()
 
@@ -44,16 +98,10 @@ client = SSHClient()
 # this key check is important to prevent e.g. MitM-attacks
 host_keys_filepath = Path(__file__).parent.joinpath("host_keys")
 logging.info("host_keys_filepath: %s", host_keys_filepath.resolve())
-client.load_host_keys(host_keys_filepath)
+client.load_host_keys(str(host_keys_filepath.resolve()))
 
-
-host = os.getenv("SUL_SSH_IP")
-port = int(os.getenv("SUL_SSH_PORT"))
-logging.info("establishing SSH connection to %s:%s...", host, port)
-client.connect(hostname=host, port=port,
-               username=os.getenv("SUL_SSH_USER", "root"),
-               key_filename=os.getenv("SUL_SSH_KEY")
-               )
+logging.info("establishing SSH connection to %s@%s:%s...", username, host, port)
+client.connect(hostname=host, port=port, username=username, key_filename=os.getenv("SUL_SSH_KEY"))
 
 logging.info("open TTY shell...")
 channel = client.invoke_shell()
